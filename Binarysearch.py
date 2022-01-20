ecf04e3d35836ecb839a783540da8412babaccbc
Dev prasanna <70941315+Devprasanna1905@users.noreply.github.com>
2022-01-20 11:46:56 +0530

    Create Binarysearch.py

---
+++ Binarysearch.py
@@ -0,0 +1,17 @@
+def binarysearch(arr,n):
+    first=0
+    last=len(arr)-1
+    while first<last:
+        mid=(first+last)//2
+        if arr[mid]==n:
+            return True
+        else:
+            if n<arr[mid]:
+                last=mid-1
+            else:
+                first=mid+1
+    return False
+
+arr=[1,2,3,4,5,6,7,8]
+n=6
+print(binary(arr,n))
